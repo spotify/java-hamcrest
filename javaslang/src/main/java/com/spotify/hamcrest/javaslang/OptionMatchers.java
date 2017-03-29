34329fe6813da988591ef52fa0ebe1c9c5f99529
Evan Shrubsole <eshr@spotify.com>
2017-03-29 17:24:05 -0400

    Add Javaslang module
    This includes the first matches, for `Option` type

---
+++ javaslang/src/main/java/com/spotify/hamcrest/javaslang/OptionMatchers.java
@@ -0,0 +1,60 @@
+/*
+ * -\-\-
+ * hamcrest-jackson
+ * --
+ * Copyright (C) 2017 Spotify AB
+ * --
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * -/-/-
+ */
+
+package com.spotify.hamcrest.javaslang;
+
+import javaslang.control.Option;
+import org.hamcrest.Matcher;
+
+import static org.hamcrest.core.IsAnything.anything;
+
+public final class OptionMatchers {
+  private OptionMatchers() {
+  }
+
+  /**
+   * Returns a matcher matching an {@link Option} that is empty
+   *
+   * @param <T> The type of the {@link Option}
+   * @return A matcher matching an {@link Option<T>} that is empty
+   */
+  public static <T> Matcher<Option<T>> emptyOption() {
+    return new EmptyOption<>();
+  }
+
+  /**
+   * Returns a matcher matching an {@link Option} that has a defined value
+   * @return A matcher matching an {@link Option} that has a defined value
+   */
+  public static Matcher<Option<?>> definedOption() {
+    return new DefinedOption<>(anything());
+  }
+
+  /**
+   * Returns a matcher matching an {@link Option} defined with value matching matcher.
+   * The matcher will not match an empty {@link Option}
+   * @param matcher The matcher for the Option's value
+   * @param <T> The type of the {@link Option}
+   * @return A matcher matching an {{@link Option} with a value matching matcher
+   */
+  public static <T> Matcher<Option<? extends T>> definedOption(final Matcher<T> matcher) {
+    return new DefinedOption<>(matcher);
+  }
+}
