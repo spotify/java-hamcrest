8de98f1c552a824bffac0513c68dc2833ae7e002
David FlemstrÃÂ¶m <dflemstr@spotify.com>
2017-07-26 22:00:31 +0200

    Prevent complex lambdas from being used in `.where(..., ...)`

--- pojo/src/main/java/com/spotify/hamcrest/pojo/IsPojo.java
+++ pojo/src/main/java/com/spotify/hamcrest/pojo/IsPojo.java
@@ -20,7 +20,6 @@
 
 package com.spotify.hamcrest.pojo;
 
-import static com.spotify.hamcrest.pojo.LambdaUtils.serializeLambda;
 import static java.util.Objects.requireNonNull;
 
 import com.google.common.base.CaseFormat;
@@ -28,6 +27,12 @@ import com.google.common.base.Joiner;
 import com.google.common.base.Splitter;
 import com.google.common.collect.ImmutableMap;
 import com.spotify.hamcrest.util.DescriptionUtils;
+import java.lang.invoke.SerializedLambda;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.security.AccessController;
+import java.security.PrivilegedActionException;
+import java.security.PrivilegedExceptionAction;
 import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.function.Consumer;
@@ -57,24 +62,29 @@ public class IsPojo<A> extends TypeSafeDiagnosingMatcher<A> {
   }
 
   public <T> IsPojo<A> where(ValueProvider<A, T> valueProvider, Matcher<T> valueMatcher) {
+    final SerializedLambda serializedLambda = serializeLambda(valueProvider);
+
+    ensureDirectMethodReference(serializedLambda);
+
+    final String implMethodName = serializedLambda.getImplMethodName();
     return whereWithoutTypeSafety(
         new NamedLambdaValueSupplier<>(
-            serializeLambda(valueProvider).getImplMethodName(),
+            implMethodName,
             valueProvider),
         valueMatcher);
   }
 
   public IsPojo<A> withProperty(String property, Matcher<?> valueMatcher) {
     return where("get" + CaseFormat.LOWER_CAMEL.to(CaseFormat.UPPER_CAMEL, property),
-                 valueMatcher);
+        valueMatcher);
   }
 
   private IsPojo<A> whereWithoutTypeSafety(Function<A, ?> valueSupplier, Matcher<?> valueMatcher) {
     return new IsPojo<>(cls,
-                        ImmutableMap.<Function<A, ?>, Matcher<?>>builder()
-                            .putAll(methodMatchers)
-                            .put(valueSupplier, valueMatcher)
-                            .build());
+        ImmutableMap.<Function<A, ?>, Matcher<?>>builder()
+            .putAll(methodMatchers)
+            .put(valueSupplier, valueMatcher)
+            .build());
   }
 
   @Override
@@ -138,4 +148,57 @@ public class IsPojo<A> extends TypeSafeDiagnosingMatcher<A> {
         .appendText("\n");
   }
 
+  /**
+   * Method uses serialization trick to extract information about lambda,
+   * to give understandable name in case of mismatch.
+   *
+   * @param lambda lambda to extract the name from
+   * @return a serialized version of the lambda, containing useful information for introspection
+   */
+  private static SerializedLambda serializeLambda(final Object lambda) {
+    requireNonNull(lambda);
+
+    final Method writeReplace;
+    try {
+      writeReplace = AccessController.doPrivileged((PrivilegedExceptionAction<Method>) () -> {
+        Method method = lambda.getClass().getDeclaredMethod("writeReplace");
+        method.setAccessible(true);
+        return method;
+      });
+    } catch (PrivilegedActionException e) {
+      throw new IllegalStateException("Cannot serialize lambdas in unprivileged context", e);
+    }
+
+    try {
+      return (SerializedLambda) writeReplace.invoke(lambda);
+    } catch (ClassCastException | IllegalAccessException | InvocationTargetException e) {
+      throw new IllegalArgumentException(
+          "Could not serialize as a lambda (is it a lambda?): " + lambda, e);
+    }
+  }
+
+  private static void ensureDirectMethodReference(final SerializedLambda serializedLambda) {
+    final Method targetMethod;
+    try {
+      final Class<?> implClass = Class.forName(serializedLambda.getImplClass().replace('/', '.'));
+      targetMethod = findMethodByName(implClass, serializedLambda.getImplMethodName());
+    } catch (NoSuchMethodException | ClassNotFoundException e) {
+      throw new IllegalStateException(
+          "serializeLambda returned a SerializedLambda pointing to an invalid class/method", e);
+    }
+
+    if (targetMethod.isSynthetic()) {
+      throw new IllegalArgumentException("The supplied lambda is not a direct method reference");
+    }
+  }
+
+  private static Method findMethodByName(final Class<?> cls, final String methodName)
+      throws NoSuchMethodException {
+    for (final Method method : cls.getDeclaredMethods()) {
+      if (method.getName().equals(methodName)) {
+        return method;
+      }
+    }
+    throw new NoSuchMethodException("No method " + methodName + " on " + cls);
+  }
 }
